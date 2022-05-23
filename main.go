b196b5c6db88b4c520d698fb2c21d78fd0b3dc42
mostafahassan0974 <mostafahassan097@gmail.com>
2022-05-24 01:59:18 +0200

    First Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World, From K8s Cluster")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
